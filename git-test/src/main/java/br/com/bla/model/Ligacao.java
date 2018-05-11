7282bc16057d5be90cbe490a9cc40e55200cebf1
Alex <alex.sbrana@gssbr.com.br>
2018-05-11 20:34:33 -0300

    Construtor e getter/setters

--- git-test/src/main/java/br/com/bla/model/Ligacao.java
+++ git-test/src/main/java/br/com/bla/model/Ligacao.java
@@ -1,9 +1,40 @@
 package br.com.bla.model;
 
 public class Ligacao {
-	
+
 	private Long id;
 	private Long numero;
-	private String endereço;	
+	private String endereço;
+	
+	public Ligacao(Long id, Long numero, String endereço) {
+		super();
+		this.id = id;
+		this.numero = numero;
+		this.endereço = endereço;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Long numero) {
+		this.numero = numero;
+	}
+
+	public String getEndereço() {
+		return endereço;
+	}
+
+	public void setEndereço(String endereço) {
+		this.endereço = endereço;
+	}
 
 }
